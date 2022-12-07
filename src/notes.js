65b3cc75ce1b6e79644912e6e3745f573f5a967d
B-8128 <brijeshvadaliya1@gmail.com>
2022-12-07 11:42:01 +0530

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,30 @@
+const starterNotes = [
+    {
+      key: 1,
+      title: "What is React?",
+      
+      content:
+        "React is a front-end and open-source JavaScript library which is useful in developing user interfaces specifically for applications with a single page. "
+    },
+    {
+      key: 2,
+      title: "What is JSX?",
+     
+      content:
+        "JSX stands for JavaScript XML. It allows us to write HTML inside JavaScript and place them in the DOM without using functions like appendChild( ) or createElement( )."
+    },
+    {
+      key: 3,
+      title: "What is React Hooks?",
+      content:
+        "React Hooks are the built-in functions that permit developers for using the state and lifecycle methods within React components."
+    },
+    {
+      key: 4,
+      title: "How does the performance of using Hooks will differ in comparison with the classes?",
+      content:
+        "React Hooks will avoid a lot of overheads such as the instance creation, binding of events, etc., that are present with classes."
+    }
+  ];
+  
+export default starterNotes;
